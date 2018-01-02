d81963f7c4fc5ff14da86002bad864a2a2d835c8
Brian Buchalter <bal711@gmail.com>
2018-01-02 12:04:20 -0700

    Add StatusBar to main view
    This will keep our content from bumping up against the status bar.

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Constants } from 'expo'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <CardsStatusBar barStyle="light-content" />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
+}
+
+function CardsStatusBar({...props}) {
+  return(
+    <View style={{height: Constants.statusBarHeight}}>
+      <StatusBar translucent {...props} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
