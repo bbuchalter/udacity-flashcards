0d6c51c0ee97d7e8ea69c47b03fd74b0e08d1d50
Brian Buchalter <bal711@gmail.com>
2018-01-02 14:47:53 -0700

    Create UI for NewDeck screen
    Next step is to wire this up to some state.

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
 export default class NewDeck extends React.Component {
+  state = {
+    deckName: ''
+  }
+
+  _submit() {
+    console.log('creating deck named ', this.state.deckName)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>New Deck Screen</Text>
+        <Text>What is the title of your deck?</Text>
+        <TextInput
+          value={this.state.deckName}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1, width: 100}}
+          onChangeText={(deckName) => this.setState({deckName})} />
+        <Button
+          onPress={() => this._submit()}
+          title="Create Deck"/>
       </View>
     );
   }
