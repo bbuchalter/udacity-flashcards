af9d2e96b9e81ecacab33fcc8ce30b338891f4cf
Brian Buchalter <bal711@gmail.com>
2018-01-03 16:06:13 -0700

    Toggle between showing answer and question

--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -3,19 +3,29 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 import { connect } from 'react-redux'
 
 class QuizQuestion extends React.Component {
+  state = {
+    showQuestion: true
+  }
+
   static navigationOptions = () => ({
     title: "Quiz"
   })
+
   render() {
     const deckName = this.props.navigation.state.params.deck
     const cards = this.props.cards.filter((card) => card.deck === deckName)
     const cardIndex = this.props.navigation.state.params.cardIndex
     const question = cards[cardIndex].question
+    const answer = cards[cardIndex].answer
 
     return (
       <View style={styles.container}>
         <Text>{cardIndex + 1} of {cards.length}</Text>
-        <Text>{question}</Text>
+        {this.state.showQuestion
+          ? <View><Text>{question}</Text><Button onPress={() => this.setState({showQuestion: false})} title="Show Answer" /></View>
+          : <View><Text>{answer}</Text><Button onPress={() => this.setState({showQuestion: true})} title="Show Question" /></View>
+        }
+
       </View>
     );
   }
